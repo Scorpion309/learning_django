0406a653049d4576bea9dec8bd0c4e40d9c10ffd
Scorpion309 <Tustin@tut.by>
2021-11-04 15:56:23 +0300

    all_materials links

--- lesson/urls.py
+++ lesson/urls.py
@@ -6,4 +6,6 @@ app_name = 'lesson'
 
 urlpatterns = [
     path('', views.all_materials, name='all_materials'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>',
+         views.material_details, name='material_details'),
 ]
